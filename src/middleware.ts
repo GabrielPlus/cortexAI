b2a0d44d9763273a12ae797b1a190de8db73ea10
GabrielPlus <gabrielpius0311@gmail.com>
2024-06-05 22:20:52 +0300

    clerk auth

---
+++ src/middleware.ts
@@ -0,0 +1,13 @@
+import { authMiddleware } from '@clerk/nextjs' 
+
+export default authMiddleware({ 
+  publicRoutes: ['/', '/auth(.*)', '/portal(.*)', '/images(.*)'], 
+  ignoredRoutes: ['/chatbot'], 
+
+}) 
+
+
+export const config = { 
+  matcher: ['/((?!.+.[w]+$|_next).*)', '/', '/(api|trpc)(.*)'], 
+
+} 
