da249349750a597b51d09cca4fececd7fc9a45e1
GabrielPlus <gabrielpius0311@gmail.com>
2024-06-10 22:51:47 +0300

    landing

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,25 @@
-import NavBar from "@/components/navbar";
-import { Button } from "@/components/ui/button";
-import Image from "next/image";
+// import { onGetBlogPosts } from '@/actions/landing'
+import NavBar from '@/components/navbar'
+import { Button } from '@/components/ui/button'
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardFooter,
+  CardHeader,
+  CardTitle,
+} from '@/components/ui/card'
+import { pricingCards } from '@/constants/landing-page'
+import clsx from 'clsx'
+import { ArrowRightCircleIcon, Check } from 'lucide-react'
+import Image from 'next/image'
+import Link from 'next/link'
+// import parse from 'html-react-parser'
+// import { getMonthName } from '@/lib/utils'
 
-export default function Home() {
+export default async function Home() {
   return (
-<main>
+    <main>
       <NavBar />
       <section>
         <div className="flex items-center justify-center flex-col mt-[80px] gap-4 ">
@@ -19,7 +34,7 @@ export default function Home() {
             className="max-w-lg object-contain"
           />
           <p className="text-center max-w-[500px]">
-            Your AI powered sales assistant! Embed Cortex AI into any website
+            Your AI powered sales assistant! Embed Corinna AI into any website
             with just a snippet of code!
           </p>
           <Button className="bg-orange font-bold text-white px-4">
@@ -34,6 +49,62 @@ export default function Home() {
           />
         </div>
       </section>
+      <section className="flex justify-center items-center flex-col gap-4 mt-10">
+        <h2 className="text-4xl text-center"> Choose what fits you right</h2>
+        <p className="text-muted-foreground text-center max-w-lg">
+          Our straightforward pricing plans are tailored to meet your needs. If
+          {" you're"} not ready to commit you can get started for free.
+        </p>
+      </section>
+      <div className="flex  justify-center gap-4 flex-wrap mt-6">
+        {pricingCards.map((card) => (
+          <Card
+            key={card.title}
+            className={clsx('w-[300px] flex flex-col justify-between', {
+              'border-2 border-primary': card.title === 'Unlimited',
+            })}
+          >
+            <CardHeader>
+              <CardTitle className="text-orange">{card.title}</CardTitle>
+              <CardDescription>
+                {pricingCards.find((c) => c.title === card.title)?.description}
+              </CardDescription>
+            </CardHeader>
+            <CardContent>
+              <span className="text-4xl font-bold">{card.price}</span>
+              <span className="text-muted-foreground">
+                <span>/ month</span>
+              </span>
+            </CardContent>
+            <CardFooter className="flex flex-col items-start gap-4">
+              <div>
+                {card.features.map((feature) => (
+                  <div
+                    key={feature}
+                    className="flex gap-2"
+                  >
+                    <Check />
+                    <p>{feature}</p>
+                  </div>
+                ))}
+              </div>
+              <Link
+                href={`/dashbord?plan=${card.title}`}
+                className="bg-[#f3d299] border-orange border-2 p-2 w-full text-center font-bold rounded-md"
+              >
+                Get Started
+              </Link>
+            </CardFooter>
+          </Card>
+        ))}
+      </div>
+
+      <section className="flex justify-center items-center flex-col gap-4 mt-28">
+        <h2 className="text-4xl text-center">News Room</h2>
+        <p className="text-muted-foreground text-center max-w-lg">
+          Explore our insights on AI, technology, and optimizing your business.
+        </p>
+      </section>
     </main>
-  );
+  )
 }
--- src/components/navbar/index.tsx
+++ src/components/navbar/index.tsx
@@ -1,6 +1,6 @@
 import Image from 'next/image'
 import * as React from 'react'
-import { Button } from '../ui/button'
+// import { Button } from '../ui/button'
 import Link from 'next/link'
 
 function NavBar() {
