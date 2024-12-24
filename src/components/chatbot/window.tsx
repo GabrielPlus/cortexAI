ece3571f8835f0e277440733065e6434f120d15c
GabrielPlus <gabrielpius0311@gmail.com>
2024-12-24 23:57:41 +0000

    chatbot open ai

--- src/components/chatbot/window.tsx
+++ src/components/chatbot/window.tsx
@@ -4,6 +4,19 @@ import { UseFormRegister } from 'react-hook-form';
 import { Avatar, AvatarFallback, AvatarImage } from '../ui/avatar';
 import RealTimeMode from './real-time';
 import Image from 'next/image';
+import TabsMenu from '../tabs';
+import ChatIcon from '@/icons/chat-icon';
+import { BOT_TABS_MENU } from '@/constants/menu';
+import { TabsContent } from '../ui/tabs';
+import { Separator } from '../ui/separator';
+import Bubble from './bubble';
+import { Responding } from './responding';
+import { Input } from '../ui/input';
+import { Button } from '../ui/button';
+import { Paperclip, Send } from 'lucide-react';
+import { Label } from '../ui/label';
+import { CardDescription, CardTitle } from '../ui/card';
+import Accordion from '../accordian';
 
 type Props = {
   errors: any;
@@ -89,6 +102,80 @@ const BotWindow = forwardRef<HTMLDivElement, Props>(
             />
           </div>
         </div>
+        <TabsMenu
+          triggers={BOT_TABS_MENU}
+          className="bg-transparent border-[1px] border-border m-2"
+        >
+
+        <TabsContent value="chat">
+            <Separator orientation="horizontal" />
+            <div className="flex flex-col h-full">
+              <div
+                style={{
+                  background: theme || '',
+                  color: textColor || '',
+                }}
+                className="px-3 flex h-[400px] flex-col py-5 gap-3 chat-window overflow-y-auto"
+                ref={ref}
+              >
+                {chats.map((chat, key) => (
+                  <Bubble
+                    key={key}
+                    message={chat}
+                  />
+                ))}
+                {onResponding && <Responding />}
+              </div>
+              <form
+                onSubmit={onChat}
+                className="flex px-3 py-1 flex-col flex-1 bg-porcelain"
+              >
+                <div className="flex justify-between">
+                  <Input
+                    {...register('content')}
+                    placeholder="Type your message..."
+                    className="focus-visible:ring-0 flex-1 p-0 focus-visible:ring-offset-0 bg-porcelain rounded-none outline-none border-none"
+                  />
+                  <Button
+                    type="submit"
+                    className="mt-3"
+                  >
+                    <Send />
+                  </Button>
+                </div>
+                <Label htmlFor="upload">
+                  <Paperclip />
+                  <Input
+                    {...register('image')}
+                    type="file"
+                    id="upload"
+                    className="hidden"
+                  />
+                </Label>
+              </form>
+            </div>
+          </TabsContent>
+          <TabsContent value="helpdesk">
+            <div className="h-[485px] overflow-y-auto overflow-x-hidden p-4 flex flex-col gap-4">
+              <div>
+                <CardTitle>Help Desk</CardTitle>
+                <CardDescription>
+                  Browse from a list of questions people usually ask.
+                </CardDescription>
+              </div>
+              <Separator orientation="horizontal" />
+
+              {helpdesk.map((desk) => (
+                <Accordion
+                  key={desk.id}
+                  trigger={desk.question}
+                  content={desk.answer}
+                />
+              ))}
+            </div>
+          </TabsContent>
+        </TabsMenu>
+
       </div>
     );
   }
